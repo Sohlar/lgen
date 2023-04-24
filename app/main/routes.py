4a1eaceb0f59c3db9205f0077be19e2e75e0381a
Sohlar <drew@robosim.com>
2023-04-23 20:30:19 -0700

    Added engine selector and index html

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,21 +4,24 @@ from app.main import main_bp
 from app.main.forms import LoginForm
 from app.main.models import User
 from app import db
-from services.search_engines import GoogleSearch, BingSearch
-from services.search_engine_factory import SearchEngineFactory
-from services.utils import search_and_record, drop_non_results
+from app.services.search_engines import GoogleSearch, BingSearch
+from app.services.search_engine_factory import SearchEngineFactory
+from app.services.utils import search_and_record, drop_non_results
 
 @main_bp.route('/')
 @main_bp.route('/index', methods=['GET', 'POST'])
 def index():
+    results = []
     if request.method == 'POST':
         query = request.form.get('query')
         engine = request.form.get('engine')
 
+        # Create a search engine using the factory
         search_engine = SearchEngineFactory().create_search_engine(engine)
+
+        # Perform the search and record the results
         results = search_and_record(search_engine, query)
-
-
+    #Process the results and render them in the template
     return render_template('index.html', results=results, title='Home')
 
 @main_bp.route('/login', methods=['GET', 'POST'])
--- app/services/config.py
+++ app/services/config.py
@@ -1,17 +1,20 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 GOOG_API_KEY = os.environ.get("GOOG_API_KEY")
 if not GOOG_API_KEY:
     raise ValueError('GOOG_API_KEY environment variable not set')
 
-GOOGLE_CX = os.environ.get("GOOG_CX_Key")
+GOOGLE_CX = os.environ.get("GOOG_CX_KEY")
 if not GOOGLE_CX:
     raise ValueError('GOOGLE_CX environment variable not set')
 
 BING_API_KEY = os.environ.get("BING_API_KEY")
 if not BING_API_KEY:
     raise ValueError('BING_API_KEY environment variable not set')
-
+'''
 EBAY_API_KEY = os.environ.get("EBAY_API_KEY")
 if not EBAY_API_KEY:
     raise ValueError('EBAY_API_KEY environment variable not set')
@@ -21,3 +24,4 @@ if not GMAPS_API_KEY:
     raise ValueError('GMAPS_API_KEY environment variable not set')
 
 EBAY_APP_ID = 'YOUR_EBAY_APP_ID'
+'''
