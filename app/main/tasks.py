4bcefec1cef5131f97153d3069e4fdbecea8e25e
Sohlar <drew@robosim.com>
2023-07-25 13:06:02 -0700

    Update tasks.py

--- app/main/tasks.py
+++ app/main/tasks.py
@@ -16,34 +16,36 @@ def search_engine_task(self, engine_str, search_history_id, query, cost):
 
 #    results2 = 
     print('RUNNING TASK NOWWWWWW')
-    for result in results:
-        if (result['email'] or result['phone']):
-            emails = result['email'] 
-            phones = result['phone'] 
-            print(emails)
-            print(phones)
-            print('---------\n')
-            search_result = SearchResult(search_history_id=search_history_id, url=result['url'])
-            print(f'*Search Result: {search_result}')
-            db.session.add(search_result)
-            db.session.flush()
-            contact_info = ContactInfo(search_result_id=search_result.id)
-            print(f'*Contact Info: {contact_info}')
-            db.session.add(contact_info)
-            db.session.flush()
+    try:
+        for result in results:
+            if (result['email'] or result['phone']):
+                emails = result['email'] 
+                phones = result['phone'] 
+                print(emails)
+                print(phones)
+                print('---------\n')
+                search_result = SearchResult(search_history_id=search_history_id, url=result['url'])
+                print(f'*Search Result: {search_result}')
+                db.session.add(search_result)
+                contact_info = ContactInfo(search_result_id=search_result.id)
+                print(f'*Contact Info: {contact_info}')
+                db.session.add(contact_info)
+                #Not sure why we are looping through run debugger to check if emails = []
+                for email_addr in emails:
+                    email = Email(contact_info_id=contact_info.id, email=email_addr)
+                    db.session.add(email)
 
-            #Not sure why we are looping through run debugger to check if emails = []
-            for email_addr in emails:
-                email = Email(contact_info_id=contact_info.id, email=email_addr)
-                db.session.add(email)
-                db.session.flush()
+                for phone_addr in phones:
+                    phone = Phone(contact_info_id=contact_info.id, phone=phone_addr)
+                    db.session.add(phone)
 
-            for phone_addr in phones:
-                phone = Phone(contact_info_id=contact_info.id, phone=phone_addr)
-                db.session.add(phone)
-                db.session.flush()
-            print('Search Done')
-    db.session.commit()
+                print('Search Done')
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print("An error Occurred, transaction rolled back", e)
+    finally:
+        db.session.close()
     current, peak = tracemalloc.get_traced_memory()
     logger.debug(f"Current memory is {current / 10**6}MB; Peak was {peak / 10**6}MB")
     tracemalloc.stop()
