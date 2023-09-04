e883c9346887538877fea6fd0b5f943ab8b3b9f6
Drew Springfield <drew@robosim.com>
2023-09-03 23:02:30 -0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -21,9 +21,9 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ['MAIL_USERNAME']
-    MAIL_DEFAULT_SENDER = os.environ['MAIL_DEFAULT_SENDER']
-    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
