0c9811326e244bfe546d3139288cf8a546861a7d
Drew Springfield <drew@robosim.com>
2023-09-08 22:58:35 -0700

    kh

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from app import create_app
+import cProfile
 
 app = create_app()
 
 if __name__ == '__main__':
-    
+    profiler = cProfile.Profile()
+    profiler.enable()
+    app.run(host='0.0.0.0', port=5001, debug=True)
+    profiler.disable()
+    profiler.print_stats(sort='time') 
     app.run(host='0.0.0.0', port=5001, debug=True)
